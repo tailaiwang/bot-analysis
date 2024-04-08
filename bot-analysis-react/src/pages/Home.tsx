7c729c9882273bb1402a4cb76cadc236b97852af
Tailai Wang <tailaiwang01@gmail.com>
2024-04-08 02:04:55 -0400

    edit home page

--- bot-analysis-react/src/pages/Home.tsx
+++ bot-analysis-react/src/pages/Home.tsx
@@ -18,11 +18,10 @@ const Home: React.FC = () => {
           <div className='col-md-10 mx-auto lead'>
             <h2 className="content-subtitle text-center mb-4"> About our Project </h2>
             <p className='content-paragraph'>
-            If you're an avid user of a social media platform, it's very likely you've come across an account that seems suspiciously like a bot.
-      With the recent degradation of the Twitter platform, this chance of encountering a bot has never been higher, and being Twitter users ourselves,
-      our team was keenly aware of this bot problem. We started our research project in early February with the goal of better understanding the whole bot problem,
-      and what kind of impact bots have not just on Twitter, but also on other popular platforms like Reddit, Facebook, and Instagram. In particular, we 
-      set out to try and answer the following questions:
+              If you're an avid user of a social media platform, it's very likely you've come across an account that seems suspiciously like a bot.
+              With recent events like the degradation of the Twitter platform and increased post-IPO scrutiny on Reddit, the bot problem seems to be under a spotlight. Being Twitter/Reddit users ourselves,
+              our team was keenly aware of this bot problem and had many anecdotes of spam, scams, and toxicity. We started our research project in early February with the goal of better understanding the whole bot problem,
+              and what kind of impact bots have not just on Twitter & Reddit, but also on other popular platforms like Facebook and Instagram. In particular, we set out to try and answer the following questions:
             </p>
             <ol className='content-list'>
               <li>What is the prevalence of the bot problem across social media platforms?</li>
@@ -32,14 +31,14 @@ const Home: React.FC = () => {
               <li>What are common industry KPIs for measuring bots and the problem(s) they cause?</li>
             </ol>
             <p className='content-paragraph'>
-            Below you will see multiple sections breaking down our research for answering these questions. 
-            To understand the problem, we have created a chapter on the background of the problem. 
-            In this section you’ll be able to better understand the impact bots have on social media platforms, who’s creating these bots,
-            and for what reasons are they being created. In addition to our secondary research, our team wanted to perform 
-            some of our own research using Reddit’s platform to see how effective existing tools are at detecting bots and
-            how their performance differs. This primary research will be covered in the research section.
-              Lastly, if you’re interested in what current industry players like Facebook are doing to mitigate this problem and
-              how they’re measuring the problem themselves, the state of the industry section covers this in detail.
+              Below you will see multiple sections breaking down our research for answering these questions.
+              To understand the problem, we have created a chapter on the background of the problem.
+              In this section you’ll be able to better understand the impact bots have on social media platforms, who’s creating these bots,
+              and for what reasons are they being created.  Next, the state of the industry section covers what current industry players like Facebook are doing to mitigate this problem and
+              how they’re measuring the problem themselves. Finally, in addition to our secondary research, our team wanted to perform
+              some of our own research using Reddit’s platform to see how effective existing tools are at detecting bots and
+              how their performance differs. This primary research will be covered in the analysis section.
+
             </p>
           </div>
           <div className="content-subtitle text-center mb-4" id="findings">
